e703fa2bdf602c196c413dd5db0b1f139093496f
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2023-09-23 11:33:40 +0300

    feat: add terraform-modules git submodule, add terraform infrastructure

---
+++ terraform/dev/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "dns_service_name" {
+  type = string
+  default = "auth"
+}
+
+variable "env" {
+  type = string
+  default = "dev"
+}
+
+variable "feature" {
+  type = string
+  default = "master"
+}
