d5f0cd19d1245b9fbbd73c8be72ebd77b470c1a6
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2024-02-15 23:30:20 +0200

    feat(email verification): add verify success/error views

---
+++ src/views/EmailVerificationSuccessView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="email-verification-success-page">
+    <MovingBackground class="email-verification-success-page__bg-container" />
+
+   <div class="email-verification-success-page__container">
+      <div class="email-verification-success-page__container-inner">
+        <span class="icon">✅</span> {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router';
+
+// components
+import MovingBackground from '@/components/layout/MovingBackground';
+
+export default {
+  components: {
+    MovingBackground
+  },
+
+  setup() {
+    document.title = 'Email verified | iMokhonko';
+
+    const router = useRouter();
+    const { query } = useRoute();
+
+    if(!query?.message) {
+      router.push({
+        name: 'login',
+        query
+      });
+    }
+
+    return {
+      message: query?.message
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.email-verification-success-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  .header {
+    font-size: 24px;
+  }
+
+  &__bg-container {
+    width: 100%;
+    max-width: 500px;
+    flex-shrink: 0;
+  }
+
+  &__container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px 0px 30px 0px rgba(0,0,0,0.4);
+    background: #fff;
+    position: relative;
+
+    &-inner {
+      max-width: 350px;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      row-gap: 24px;
+    }
+  }
+}
+</style>
